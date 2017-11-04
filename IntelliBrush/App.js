475998eef8283cff1d0d2eed712b57a45b7ed46d
Lisa Campbell <campb215@purdue.edu>
2017-11-04 18:33:44 -0400

    Set up basic react native project

---
+++ IntelliBrush/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Container, Button, Content, Header, H1, Title, Body, Left, Icon, Right, Spinner} from 'native-base';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Testo</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
