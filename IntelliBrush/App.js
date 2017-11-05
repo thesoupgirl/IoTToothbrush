9eac98ab9f8a8f18bd23c2c266b9bded1d8aab20
Lisa Campbell <campb215@purdue.edu>
2017-11-05 00:30:35 -0400

    Added Overview and Details pages, navigates from login to details pages now

--- IntelliBrush/App.js
+++ IntelliBrush/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import Login from './Login';
+import Overview from './Overview';
+import { StackNavigator } from 'react-navigation';
 import { Container, Button, Content, Header, H1, Title, Body, Left, Icon, Right, Spinner} from 'native-base';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-       <Login />
+       <Nav />
     );
   }
 }
+
+export const Nav = StackNavigator({
+  Login: { screen: Login },
+  Overview: { screen: Overview },
+});
+
+AppRegistry.registerComponent('App', () => Nav);
 
 const styles = StyleSheet.create({
   container: {
--- IntelliBrush/Login.js
+++ IntelliBrush/Login.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Container from './components/Container';
 import Button from './components/Button';
 import Label from './components/Label';
+import Overview from './Overview';
  
 import {
   StyleSheet,
@@ -14,16 +15,35 @@ import {
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class Login extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      username: '',
+      password: '',
+    }
+  }
+
+  clearText = () => {
+    this._textInput.setNativeProps({text: ''});
+    this._textInput2.setNativeProps({text: ''});
+  }
+
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+
   render() {
+  	const { navigate } = this.props.navigation;
     return (
         <ScrollView style={styles.scroll}>
         	<Container>
     			<Label text="Username or Email" />
-    			<TextInput style={styles.textInput}/>
+    			<TextInput ref={component => this._textInput = component} style={styles.textInput} onChangeText={(text) => this.setState({username:text})}/>
 			</Container>
 			<Container>
     			<Label text="Password" />
-    			<TextInput secureTextEntry={true} style={styles.textInput} />
+    			<TextInput ref={component => this._textInput2 = component} secureTextEntry={true} style={styles.textInput} onChangeText={(text) => this.setState({password:text})}/>
 			</Container>
 			<Container>
 				<Button styles={{button: styles.transparentButton}} onPress={this.press.bind(this)}>
@@ -31,24 +51,34 @@ export default class Login extends Component {
     		</Container>
     		<View style={styles.footer}>
 	    		<Container>
-	        		<Button label="Sign In" styles={{button: styles.primaryButton, label: styles.buttonWhiteText}}  onPress={this.press.bind(this)} />
+	        		<Button label="Sign In" styles={{button: styles.primaryButton, label: styles.buttonWhiteText}}  onPress={() => navigate('Overview')} />
 	    		</Container>
     			<Container>
-			        <Button label="CANCEL" styles={{label: styles.buttonBlackText}} onPress={this.press.bind(this)} />
+			        <Button label="CANCEL" styles={{label: styles.buttonBlackText}} onPress={this.clearText} />
 		    	</Container>
 			</View>
         </ScrollView>
     );
   }
 
+pressSignIn() {
+	//const { navigate } = this.props.navigation;
+	//navigate('Overview');
+	console.log("m");
+
+}
+pressCancel() {
+  console.log("r");
+  this.state.username = "";
+}
 press() {
-  //execute any code here
+
 }
 }
 
 const styles = StyleSheet.create({
  	scroll: {
- 		paddingTop: 80,
+ 		paddingTop: 30,
 	    backgroundColor: '#FFFFFF',
 	    padding: 30,
 	    flexDirection: 'column'
