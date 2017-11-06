5c8fa729b098430c445ef21ee6a6777c78855118
Lisa Campbell <campb215@purdue.edu>
2017-11-06 12:49:16 -0500

    Made logout button that navigates back to sign in screen

--- IntelliBrush/Overview.js
+++ IntelliBrush/Overview.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
 import Label from './components/Label';
 import Container from './components/Container';
+import Button from './components/Button';
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ScrollView
+} from 'react-native';
+
 export default class Overview extends Component {
   render() {
+  	const { navigate } = this.props.navigation;
     return (
-        	<Container>
-    			<Label text="Overview Page" />
-			</Container>
+    	    <ScrollView style={styles.scroll}>
+	        	<Container>
+	    			<View style={styles.header}>
+	    				<Button label="Logout" styles={{button: styles.primaryButton, label: styles.buttonWhiteText}}  onPress={() => navigate('Login')} />
+	    			</View>
+				</Container>
+			</ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+	header: {
+	   marginTop: -10
+	},
+	primaryButton: {
+		paddingRight: 50,
+	    backgroundColor: '#8DDBE0'
+	},
+	buttonWhiteText: {
+	    fontSize: 20,
+	    color: '#FFF',
+	},
+	scroll: {
+ 		paddingTop: 30,
+	    backgroundColor: '#FFFFFF',
+	    padding: 30,
+	    flexDirection: 'column'
+    },
+});
