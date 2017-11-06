d83c65a1d5ca00948e20ef173e9ace507ab178fb
Lisa Campbell <campb215@purdue.edu>
2017-11-06 14:57:55 -0500

    Started making grid

--- IntelliBrush/Overview.js
+++ IntelliBrush/Overview.js
@@ -21,6 +21,24 @@ export default class Overview extends Component {
 	    				<Button label="Logout" styles={{button: styles.primaryButton, label: styles.buttonWhiteText}}  onPress={() => navigate('Login')} />
 	    			</View>
 				</Container>
+				<Label text="Latest Sessions" />
+				<View style={{flex: 1, flexDirection: 'row'}}>
+			        <View style={{width: 100, height: 30, backgroundColor: 'lightgrey'}} >
+			        	<Text style={ styles.tableBlackText }>
+			        		Time Stamp
+			        	</Text>
+			        </View>
+			        <View style={{width: 100, height: 30, backgroundColor: 'lightgrey'}} >
+			        	<Text style={ styles.tableBlackText }>
+			        		Duration
+			        	</Text>
+			        </View>
+			        <View style={{width: 60, height: 30, backgroundColor: 'lightgrey'}}>
+			        	<Text style={ styles.tableBlackText }>
+			        		Grade
+			        	</Text>
+			        </View>
+			    </View>
 			</ScrollView>
     );
   }
@@ -31,11 +49,14 @@ const styles = StyleSheet.create({
 	   marginTop: -10
 	},
 	primaryButton: {
+		marginRight: 0,
+		marginLeft: 90,
+		paddingLeft: 50,
 		paddingRight: 50,
 	    backgroundColor: '#8DDBE0'
 	},
 	buttonWhiteText: {
-	    fontSize: 20,
+	    fontSize: 15,
 	    color: '#FFF',
 	},
 	scroll: {
@@ -44,4 +65,12 @@ const styles = StyleSheet.create({
 	    padding: 30,
 	    flexDirection: 'column'
     },
+    label: {
+	    color: '#0d8898',
+	    fontSize: 15
+    },
+    tableBlackText: {
+	    fontSize: 15,
+	    color: '#595856'
+	}
 });
