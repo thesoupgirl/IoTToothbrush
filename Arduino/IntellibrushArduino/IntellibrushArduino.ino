9aa4e58dd1fc5e2284a7d15e87f88446b2c32df8
Kameron Lutes <kalutes@purdue.edu>
2017-11-14 00:10:35 -0500

    cleaned up arduino code and added support for 2 accelerometers

--- Arduino/IntellibrushArduino/IntellibrushArduino.ino
+++ Arduino/IntellibrushArduino/IntellibrushArduino.ino
@@ -1,6 +1,7 @@
 #include "I2Cdev.h"
 #include <SPI.h>
 #include <SD.h>
+#include <FastLED.h>
 
 #include "MPU6050_6Axis_MotionApps20.h"
 
@@ -8,259 +9,210 @@
     #include "Wire.h"
 #endif
 
-// class default I2C address is 0x68
-// specific I2C addresses may be passed as a parameter here
-// AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
-// AD0 high = 0x69
-MPU6050 mpu;
-//MPU6050 mpu(0x69); // <-- use for AD0 high
+MPU6050 accel;
+MPU6050 accel2(0x69); // <-- use for AD0 high
 
-// uncomment "OUTPUT_READABLE_QUATERNION" if you want to see the actual
-// quaternion components in a [w, x, y, z] format (not best for parsing
-// on a remote host such as Processing or something though)
 #define OUTPUT_READABLE_QUATERNION
 
-// uncomment "OUTPUT_READABLE_EULER" if you want to see Euler angles
-// (in degrees) calculated from the quaternions coming from the FIFO.
-// Note that Euler angles suffer from gimbal lock (for more info, see
-// http://en.wikipedia.org/wiki/Gimbal_lock)
-//#define OUTPUT_READABLE_EULER
+#define RGB_CLOCK 0
+#define RGB_DATA 0
+#define NUM_RGB 1
+#define BUTTON_PIN 0
 
-// uncomment "OUTPUT_READABLE_YAWPITCHROLL" if you want to see the yaw/
-// pitch/roll angles (in degrees) calculated from the quaternions coming
-// from the FIFO. Note this also requires gravity vector calculations.
-// Also note that yaw/pitch/roll angles suffer from gimbal lock (for
-// more info, see: http://en.wikipedia.org/wiki/Gimbal_lock)
-//#define OUTPUT_READABLE_YAWPITCHROLL
+#define NUM_CYCLES 1000
 
-// uncomment "OUTPUT_READABLE_REALACCEL" if you want to see acceleration
-// components with gravity removed. This acceleration reference frame is
-// not compensated for orientation, so +X is always +X according to the
-// sensor, just without the effects of gravity. If you want acceleration
-// compensated for orientation, us OUTPUT_READABLE_WORLDACCEL instead.
-//#define OUTPUT_READABLE_REALACCEL
-
-// uncomment "OUTPUT_READABLE_WORLDACCEL" if you want to see acceleration
-// components with gravity removed and adjusted for the world frame of
-// reference (yaw is relative to initial orientation, since no magnetometer
-// is present in this case). Could be quite handy in some cases.
-//#define OUTPUT_READABLE_WORLDACCEL
-
-// uncomment "OUTPUT_TEAPOT" if you want output that matches the
-// format used for the InvenSense teapot demo
-//#define OUTPUT_TEAPOT
-
-#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+CRGB rgbled[NUM_RGB];
 bool blinkState = false;
 
 // MPU control/status vars
-bool dmpReady = false;  // set true if DMP init was successful
-uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
-uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+bool accelReady = false;  // set true if DMP init was successful
+bool accel2Ready = false;
+uint8_t accelIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t accel2IntStatus;
+uint8_t accelStatus;      // return status after each device operation (0 = success, !0 = error)
+uint8_t accel2Status;
 uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t packet2Size;
 uint16_t fifoCount;     // count of all bytes currently in FIFO
 uint8_t fifoBuffer[64]; // FIFO storage buffer
+uint16_t fifo2Count;     // count of all bytes currently in FIFO
+uint8_t fifo2Buffer[64]; // FIFO storage buffer
 
-// orientation/motion vars
-Quaternion q;           // [w, x, y, z]         quaternion container
-VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-VectorFloat gravity;    // [x, y, z]            gravity vector
-float euler[3];         // [psi, theta, phi]    Euler angle container
-float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+Quaternion quat;
 
-// packet structure for InvenSense teapot demo
-uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
-
-File myFile;
+File logFile;
 uint16_t cycles;
 long lastCycleTime = 0;
-#define NUM_CYCLES 10000
 
-// ================================================================
-// ===               INTERRUPT DETECTION ROUTINE                ===
-// ================================================================
+volatile bool accelInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+volatile bool accel2Interrupt = false;
 
-volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
-void dmpDataReady() {
-    mpuInterrupt = true;
+void accelDataReady() {
+    accelInterrupt = true;
 }
 
-
-
-// ================================================================
-// ===                      INITIAL SETUP                       ===
-// ================================================================
+void accel2DataReady() {
+    accel2Interrupt = true;
+}
 
 void setup() {
-    // join I2C bus (I2Cdev library doesn't do this automatically)
+    setupLogfile();
+    setupRGB();
+    setupI2C();
+    startCollection();
+}
+
+void setupLogfile()
+{
+    if (!SD.begin(4)) {
+      errorBlink(1);
+    }
+
+    int fileNum = 0;
+    while(SD.exists(String(fileNum))) {
+      fileNum++;
+    }
+    
+    logFile = SD.open(String(fileNum), FILE_WRITE);
+  
+    if (!logFile) {
+      errorBlink(2);
+    }
+}
+
+void setupRGB()
+{
+    FastLED.addLeds<P9813, RGB_DATA, RGB_CLOCK, RGB>(rgbled, NUM_RGB);
+}
+
+void setupI2C() {
     #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
         Wire.begin();
         TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
     #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
         Fastwire::setup(400, true);
     #endif
-
-    // initialize serial communication
-    // (115200 chosen because it is required for Teapot Demo output, but it's
-    // really up to you depending on your project)
-    Serial.begin(115200);
-    while (!Serial); // wait for Leonardo enumeration, others continue immediately
-
-     Serial.print("Initializing SD card...");
-
-    if (!SD.begin(4)) {
-      Serial.println("initialization failed!");
-      return;
+    
+    accel.initialize();
+    accel2.initialize();
+    if(!accel.testConnection() || accel2.testConnection()) {
+      errorBlink(3);
     }
-    Serial.println("initialization done.");
-
-    myFile = SD.open("test.txt", FILE_WRITE);
-  
-    // if the file opened okay, write to it:
-    if (!myFile) {
-      Serial.println("error opening test.txt");
-    }
-
-    // initialize device
-    Serial.println(F("Initializing I2C devices..."));
-    mpu.initialize();
-
-    // verify connection
-    Serial.println(F("Testing device connections..."));
-    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-
-    // wait for ready
-    Serial.println(F("\nSend any character to begin DMP programming and demo: "));
-    while (Serial.available() && Serial.read()); // empty buffer
-    while (!Serial.available());                 // wait for data
-    while (Serial.available() && Serial.read()); // empty buffer again
-
-    // load and configure the DMP
-    Serial.println(F("Initializing DMP..."));
-    devStatus = mpu.dmpInitialize();
-
-    // supply your own gyro offsets here, scaled for min sensitivity
-    mpu.setXGyroOffset(220);
-    mpu.setYGyroOffset(76);
-    mpu.setZGyroOffset(-85);
-    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
-
-    // make sure it worked (returns 0 if so)
-    if (devStatus == 0) {
-        // turn on the DMP, now that it's ready
-        Serial.println(F("Enabling DMP..."));
-        mpu.setDMPEnabled(true);
-
-        // enable Arduino interrupt detection
-        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
-        attachInterrupt(0, dmpDataReady, RISING);
-        mpuIntStatus = mpu.getIntStatus();
-
-        // set our DMP Ready flag so the main loop() function knows it's okay to use it
-        Serial.println(F("DMP ready! Waiting for first interrupt..."));
-        dmpReady = true;
-
-        // get expected DMP packet size for later comparison
-        packetSize = mpu.dmpGetFIFOPacketSize();
-    } else {
-        // ERROR!
-        // 1 = initial memory load failed
-        // 2 = DMP configuration updates failed
-        // (if it's going to break, usually the code will be 1)
-        Serial.print(F("DMP Initialization failed (code "));
-        Serial.print(devStatus);
-        Serial.println(F(")"));
-    }
-
-    // configure LED for output
-    pinMode(LED_PIN, OUTPUT);
 }
 
+void startCollection() {
+    accelStatus = accel.dmpInitialize();
+    accel2Status = accel2.dmpInitialize();
 
+    accel.setXGyroOffset(220);
+    accel.setYGyroOffset(76);
+    accel.setZGyroOffset(-85);
+    accel.setZAccelOffset(1788); // 1688 factory default for my test chip
+    accel2.setXGyroOffset(220);
+    accel2.setYGyroOffset(76);
+    accel2.setZGyroOffset(-85);
+    accel2.setZAccelOffset(1788); // 1688 factory default for my test chip
 
-// ================================================================
-// ===                    MAIN PROGRAM LOOP                     ===
-// ================================================================
+    if (accelStatus == 0) {
+        accel.setDMPEnabled(true);
+        attachInterrupt(0, accelDataReady, RISING);
+        accelIntStatus = accel.getIntStatus();
+        accelReady = true;
+        packetSize = accel.dmpGetFIFOPacketSize();
+    } else {
+        errorBlink(4);
+      }
+
+    if (accel2Status == 0) {
+        accel2.setDMPEnabled(true);
+        attachInterrupt(1, accel2DataReady, RISING);
+        accel2IntStatus = accel2.getIntStatus();
+        accel2Ready = true;
+        packet2Size = accel2.dmpGetFIFOPacketSize();
+    } else {
+        errorBlink(5);
+      }
+}
 
 void loop() {
-  if(cycles < NUM_CYCLES)
-  {
-    // if programming failed, don't try to do anything
-    if (!dmpReady) return;
+    if (!accelReady || !accel2Ready) return;
 
-    // wait for MPU interrupt or extra packet(s) available
-    while (!mpuInterrupt && fifoCount < packetSize) {
-        // other program behavior stuff here
-        // .
-        // .
-        // .
-        // if you are really paranoid you can frequently test in between other
-        // stuff to see if mpuInterrupt is true, and if so, "break;" from the
-        // while() loop to immediately process the MPU data
-        // .
-        // .
-        // .
+    while (!accelInterrupt && !accel2Interrupt && fifoCount < packetSize && fifo2Count < packet2Size) {
+      if(accelInterrupt) {
+        handleAccel();
+      }
+      
+      if(accel2Interrupt) {
+        handleAccel2();
+      }
     }
+}
 
-    // reset interrupt flag and get INT_STATUS byte
-    mpuInterrupt = false;
-    mpuIntStatus = mpu.getIntStatus();
+void handleAccel() {
+    accelInterrupt = false;
+    accelIntStatus = accel.getIntStatus();
 
-    // get current FIFO count
-    fifoCount = mpu.getFIFOCount();
+    fifoCount = accel.getFIFOCount();
 
-    // check for overflow (this should never happen unless our code is too inefficient)
-    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
-        // reset so we can continue cleanly
-        mpu.resetFIFO();
-        Serial.println(F("FIFO overflow!"));
-
-    // otherwise, check for DMP data ready interrupt (this should happen frequently)
-    } else if (mpuIntStatus & 0x02) {
-        // wait for correct available data length, should be a VERY short wait
-        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-
-        // read a packet from FIFO
-        mpu.getFIFOBytes(fifoBuffer, packetSize);
+    if ((accelIntStatus & 0x10) || fifoCount == 1024) {
+        accel.resetFIFO();
+        errorBlink(5);
+    } else if (accelIntStatus & 0x02) {
+        while (fifoCount < packetSize) fifoCount = accel.getFIFOCount();
         
-        // track FIFO count here in case there is > 1 packet available
-        // (this lets us immediately read more without waiting for an interrupt)
+        accel.getFIFOBytes(fifoBuffer, packetSize);
+        
         fifoCount -= packetSize;
 
-        #ifdef OUTPUT_READABLE_QUATERNION
-            // display quaternion values in easy matrix form: w x y z
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            Serial.print("quat\t");
-            Serial.print(q.w);
-            Serial.print("\t");
-            Serial.print(q.x);
-            Serial.print("\t");
-            Serial.print(q.y);
-            Serial.print("\t");
-            Serial.println(q.z);
-            myFile.print("quat\t");
-            myFile.print(q.w);
-            myFile.print("\t");
-            myFile.print(q.x);
-            myFile.print("\t");
-            myFile.print(q.y);
-            myFile.print("\t");
-            myFile.print(q.z);
-            myFile.print("\t");          
-            myFile.println(millis() - lastCycleTime);
-            lastCycleTime = millis();
-            cycles++;
-        #endif
-
-        // blink LED to indicate activity
-        blinkState = !blinkState;
-        digitalWrite(LED_PIN, blinkState);
+        accel.dmpGetQuaternion(&quat, fifoBuffer);
+        printToLog();
     } 
-  }
-  if(cycles == NUM_CYCLES)
-  {
-    myFile.close();
+}
+
+void handleAccel2() {
+    accel2Interrupt = false;
+    accel2IntStatus = accel.getIntStatus();
+
+    fifo2Count = accel2.getFIFOCount();
+
+    if ((accel2IntStatus & 0x10) || fifo2Count == 1024) {
+        accel2.resetFIFO();
+        errorBlink(6);
+    } else if (accel2IntStatus & 0x02) {
+        while (fifo2Count < packet2Size) fifo2Count = accel2.getFIFOCount();
+        
+        accel2.getFIFOBytes(fifo2Buffer, packet2Size);
+        
+        fifo2Count -= packet2Size;
+
+        accel2.dmpGetQuaternion(&quat, fifo2Buffer);
+        printToLog();
+    } 
+}
+
+void printToLog() {
+    logFile.print(quat.w);
+    logFile.print(",");
+    logFile.print(quat.x);
+    logFile.print(",");
+    logFile.print(quat.y);
+    logFile.print(",");
+    logFile.print(quat.z);
+    logFile.print(",");          
+    logFile.println(millis()); 
+}
+
+void errorBlink(int blinks) {
+  while(1) {
+    for(int i = 0; i < blinks; i++) {
+      rgbled[0] = CRGB::Red;
+      FastLED.show();
+      delay(250);
+      rgbled[0] = CRGB::Black;
+      FastLED.show();
+      delay(250);
+    }
+    delay(3000);
   }
 }
+
