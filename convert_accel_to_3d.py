c36c547804c40206b3d51ecb886c28da7a5958aa
Jiaxin Liu <liujiaxin2034@gmail.com>
2017-12-04 07:24:45 -0500

    Add files via upload
    converted sample datasets: http://hi.liujiaxin.info/index.html?data=points/points and http://hi.liujiaxin.info/index.html?data=another/tes1lt

---
+++ convert_accel_to_3d.py
@@ -0,0 +1,26 @@
+with open("./in.txt", "r") as filein:
+	with open("./out.json", "w") as fileout:
+		fileout.write("{\"points\": [")
+		v = [[0, 0, 0], [0, 0, 0]]
+		p = [[0, 0, 0], [0, 0, 0]]
+		first = True
+		for line in filein:
+			if first:
+				first = False
+			else:
+				fileout.write(", \n")
+			data = line.split(",")
+			accel_id = int(data[0]) - 1
+			x = float(data[2])
+			y = float(data[3])
+			z = float(data[4])
+
+			v[accel_id][0] = v[accel_id][0] + x
+			v[accel_id][1] = v[accel_id][1] + y
+			v[accel_id][2] = v[accel_id][2] + z
+
+			p[accel_id][0] = p[accel_id][0] + v[accel_id][0]
+			p[accel_id][1] = p[accel_id][1] + v[accel_id][1]
+			p[accel_id][2] = p[accel_id][2] + v[accel_id][2]
+			fileout.write("{\"cid\" : \"" + str(accel_id) + "\", \"x\" : \"" + str(x * 100) + "\", \"y\" : \"" + str(y * 100) + "\", \"z\" : \"" + str(z * 100) + "\"}")
+		fileout.write("]}")
